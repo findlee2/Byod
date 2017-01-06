e5bfbf0335f169ab976e6c0961b042765922c214
findlee2 <findlee2@nate.com>
2017-01-06 17:28:02 +0900

    2017-01-06 오후 5:27분 탑메뉴 및 CommonViewController.java 수정

--- ByodManager/src/main/java/slst/byod/api/cmm/view/CommonViewController.java
+++ ByodManager/src/main/java/slst/byod/api/cmm/view/CommonViewController.java
@@ -1,12 +1,10 @@
 package slst.byod.api.cmm.view;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import slst.byod.api.userManage.UserManageVO;
@@ -31,7 +29,7 @@ public class CommonViewController {
 	 */
 	@RequestMapping(value="/login")
 	public String loginView() throws Exception{		
-		return "userInfo/userLogin";
+		return "userLogin";
 	}
 	
 	/**
@@ -49,13 +47,55 @@ public class CommonViewController {
 	 * @return
 	 * @throws Exception
 	 */
-	@RequestMapping(value="/srchInfo/srchInfoList",  method = RequestMethod.POST)
+	@RequestMapping(value="/srchInfo/srchInfoList")
 	public ModelAndView byodManagerSrchInfoListView(HttpServletRequest request,	ModelMap model) throws Exception{
 		
-		UserManageVO userVO = (UserManageVO)request.getSession().getAttribute("userVO");
-		
+		UserManageVO userVO = (UserManageVO)request.getSession().getAttribute("userVO");		
 		model.addAttribute("userVO", userVO);
+		
 		return new ModelAndView("srchInfo/srchInfoList");
+		
 	}
 	
+	/**
+	 * 사용자관리 리스트 페이지 출력
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping(value="/userInfo/userInfoList")
+	public ModelAndView byodManagerUserInfoListView(HttpServletRequest request,	ModelMap model) throws Exception{
+		
+		UserManageVO userVO = (UserManageVO)request.getSession().getAttribute("userVO");		
+		model.addAttribute("userVO", userVO);
+		
+		return new ModelAndView("userInfo/userInfoList");
+	}
+	
+	/**
+	 * 단말기관리 리스트 페이지 출력
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping(value="/srchEqupmnt/srchEqupmntList")
+	public ModelAndView byodManagerSrchEqupmntListListView(HttpServletRequest request,	ModelMap model) throws Exception{
+		
+		UserManageVO userVO = (UserManageVO)request.getSession().getAttribute("userVO");		
+		model.addAttribute("userVO", userVO);
+		
+		return new ModelAndView("srchEqupmnt/srchEqupmntList");
+	}
+	
+	/**
+	 * 로그관리 리스트 페이지 출력
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping(value="/logInfo/logInfoList")
+	public ModelAndView byodManagerLogInfoListView(HttpServletRequest request,	ModelMap model) throws Exception{
+		
+		UserManageVO userVO = (UserManageVO)request.getSession().getAttribute("userVO");		
+		model.addAttribute("userVO", userVO);
+		
+		return new ModelAndView("logInfo/logInfoList");
+	}
 }
--- ByodManager/src/main/webapp/WEB-INF/view/jsp/include/top.jsp
+++ ByodManager/src/main/webapp/WEB-INF/view/jsp/include/top.jsp
@@ -1,22 +1,15 @@
 <%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="utf-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-<script>
-	function homeLoaction(){
-		$("#tmpForm").submit();
-	}
-</script>
 <div id="header">
-<form id="tmpForm" name="tmpForm" action="/srchInfo/srchInfoList" method="post"></form>
-
-	<h1><a onclick="homeLoaction()"><img src="<c:url value='/images/byod-mi.png' />" alt="BYOD 현장조사 업무지원 시스템 MI" /></a></h1>
+	<h1><a href="<c:url value='/srchInfo/srchInfoList'/>"><img src="<c:url value='/images/byod-mi.png' />" alt="BYOD 현장조사 업무지원 시스템 MI" /></a></h1>
 	<div>
 		<a href="#link">로그인</a>
 		<a href="#link">회원가입</a>
 	</div>
 </div>
 <div id="nav">
-	<a href="#link">업무관리</a>
-	<a href="#link">사용자관리</a>
-	<a href="#link">단말기관리</a>
-	<a href="#link">로그관리</a>
+	<a href="<c:url value='/srchInfo/srchInfoList'/>">업무관리</a>
+	<a href="<c:url value='/userInfo/userInfoList'/>">사용자관리</a>
+	<a href="<c:url value='/srchEqupmnt/srchEqupmntList'/>">단말기관리</a>
+	<a href="<c:url value='/logInfo/logInfoList'/>">로그관리</a>
 </div>
